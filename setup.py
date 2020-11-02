dca131fe08c9d16596d25879ae53ecf78adb4a99
rapixar <cudraphael@gmail.com>
2020-11-02 10:57:05 +0100

    Add License file and update setup.py
    Here, I added the MIT license to the package and included more arguments
    for the package including the metadata, python requirements, classifiers
    and description. Also added the long-description which is the README.md

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='statman',
-      version='0.1',
-      description='Gaussian distributions',
-      packages=['statman'],
-      zip_safe=False)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='statman',
+                 version='0.1.1',
+                 author="Chinenye Raphael, Ezeoke Godswill",
+                 author_email="cudraphael@gmail.com",
+                 description='Statistical Distribution',
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/Rapixar/statman_package",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires='>=3.6',
+                 )
